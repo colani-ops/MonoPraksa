fc3a9df4d8ccc28144919030ecd23ea438d7fdeb
Nikola <84501234+colani-ops@users.noreply.github.com>
2023-01-22 16:24:32 +0100

    (Day 3/4) prototype functions, have to finish & layer

--- Planets/Planets.WebAPI/Controllers/StarSystemController.cs
+++ Planets/Planets.WebAPI/Controllers/StarSystemController.cs
@@ -14,7 +14,7 @@ namespace Planets.WebAPI.Controllers
 
         string connectionString = "server = localhost; database = Planets; trusted_connection=true";
 
-        //GET Method here
+        [HttpGet] //api/Planet/get-starsystems
         [Route("api/Planet/get-starsystems")]
         public HttpResponseMessage Get()
         {
@@ -23,7 +23,7 @@ namespace Planets.WebAPI.Controllers
             {
 
                 SqlCommand command = new SqlCommand(
-                    "SELECT StarSystem.Id, StarSystem.Name FROM dbo.StarSystem," 
+                    "SELECT StarSystem.Id, StarSystem.Name FROM dbo.StarSystem,"
                     + " Planet.Id, Planet.Name, Planet.Type, Planet.Radius, Planet.Gravity FROM StarSystem"
                     + " LEFT JOIN Planet ON StarSystem.Id = Planet.StarSystemId;", connection
                     );
@@ -48,7 +48,7 @@ namespace Planets.WebAPI.Controllers
                     }
 
                     reader.Close();
-                    
+
                     connection.Close();
 
                     return Request.CreateResponse(HttpStatusCode.OK, starSystemList);
@@ -57,14 +57,93 @@ namespace Planets.WebAPI.Controllers
             }
         }
 
-        //GET By ID Here
 
-        //GET StarSystem PLANETS Here
 
-        //POST (Add StarSystem) Here
+        [HttpGet] //api/Planet/get-starsystem-id/{targetId}
+        [Route("api/Planet/get-starsystem-id/{targetId}")]
+        public HttpResponseMessage GetById(Guid targetId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                StarSystem tempStarSystem;
+                SqlCommand command = new SqlCommand("SELECT * FROM dbo.StarSystem WHERE Id = " + targetId + ";", connection);
+                connection.Open();
 
-        //UPDATE Here
+                SqlDataReader reader = command.ExecuteReader();
 
-        //DELETE By ID Here
+                if (reader.HasRows)
+                {
+                    reader.Read();
+                    tempStarSystem = new StarSystem(reader.GetGuid(0), reader.GetString(1));
+                    reader.Close();
+
+                    connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK, tempStarSystem);
+                }
+                connection.Close();
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "There is no Star System with that Id!");
+            }
+        }
+
+
+
+        [HttpGet] //api/Planet/getstarsystem-planets
+        [Route("api/Planet/get-starsystem-planets")]
+        public HttpResponseMessage GetStarSystemPlanets(Guid targetId)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand();
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                StarSystem tempStarSystem = new StarSystem();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        if (tempStarSystem.Id == null)
+                        {
+                            tempStarSystem = new StarSystem(reader.GetGuid(0), reader.GetString(1));
+                        }
+                        //add planet to tempStarSystem
+                    }
+                    reader.Close();
+                    connection.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK, tempStarSystem);
+                }
+                connection.Close();
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Empty");
+            }
+
+        }
+
+
+
+        [HttpPost] //POST (Add StarSystem) Here
+        [Route("api/Planet/add-starsystem")]
+        public HttpResponseMessage AddStarSystem() 
+        {
+
+        }
+        
+        
+        
+        [HttpPut]//UPDATE Here
+        [Route("api/Planet/update-starsystem")]
+        public HttpResponseMessage UpdateStarSystem()
+        {
+
+        }
+
+
+
+        [HttpDelete]
+        [Route("api/Planet/delete-starsystem/{targetId}")]
+        public HttpResponseMessage DeleteStarSystem(Guid targetId)
+        {
+
+        }
     }
 }
