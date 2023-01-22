a3bcdb957d74bf5d19291e90d277cef2ad1aded5
Nikola <84501234+colani-ops@users.noreply.github.com>
2023-01-22 15:57:37 +0100

    StarSystem functions prototype

--- Planets/Planets.WebAPI/Controllers/StarSystemController.cs
+++ Planets/Planets.WebAPI/Controllers/StarSystemController.cs
@@ -12,11 +12,50 @@ namespace Planets.WebAPI.Controllers
     public class StarSystemController : ApiController
     {
 
-        SqlConnection connection = new SqlConnection(
-        "server = localhost; database = Planets; trusted_connection=true"
-        );
+        string connectionString = "server = localhost; database = Planets; trusted_connection=true";
 
         //GET Method here
+        [Route("api/Planet/get-starsystems")]
+        public HttpResponseMessage Get()
+        {
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+
+                SqlCommand command = new SqlCommand(
+                    "SELECT StarSystem.Id, StarSystem.Name FROM dbo.StarSystem," 
+                    + " Planet.Id, Planet.Name, Planet.Type, Planet.Radius, Planet.Gravity FROM StarSystem"
+                    + " LEFT JOIN Planet ON StarSystem.Id = Planet.StarSystemId;", connection
+                    );
+
+                connection.Open();
+
+                List<StarSystem> starSystemList = new List<StarSystem>();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+
+                    while (reader.Read())
+                    {
+                        StarSystem tempSystem = starSystemList.Find(StarSystem => StarSystem.Id == reader.GetGuid(0));
+
+                        if (tempSystem == null)
+                        {
+                            starSystemList.Add(new StarSystem(reader.GetGuid(0), reader.GetString(1)));
+                        }
+                    }
+
+                    reader.Close();
+                    
+                    connection.Close();
+
+                    return Request.CreateResponse(HttpStatusCode.OK, starSystemList);
+                }
+                return Request.CreateResponse(HttpStatusCode.NotFound, "No Star Systems Found");
+            }
+        }
 
         //GET By ID Here
 
@@ -27,7 +66,5 @@ namespace Planets.WebAPI.Controllers
         //UPDATE Here
 
         //DELETE By ID Here
-
-
     }
 }
