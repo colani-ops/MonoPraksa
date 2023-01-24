a5094744009250e18b2d441060b670de19336b27
Nikola <84501234+colani-ops@users.noreply.github.com>
2023-01-24 13:45:25 +0100

    Day 7 - DI with Global.asax

--- Planets/Planets.WebAPI/Global.asax.cs
+++ Planets/Planets.WebAPI/Global.asax.cs
@@ -1,9 +1,17 @@
+using Autofac;
+using Autofac.Integration.WebApi;
+using Planets.Service;
+using Planets.Service.Common;
+using Planets.Repository;
+using Planets.Repository.Common;
+using Planets.WebAPI.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Routing;
+using System.Reflection;
 
 namespace Planets.WebAPI
 {
@@ -12,6 +20,23 @@ namespace Planets.WebAPI
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            var builder = new ContainerBuilder();
+
+            var config = GlobalConfiguration.Configuration;
+
+            builder.RegisterType <PlanetService>().As<IPlanetService>();
+            builder.RegisterType <PlanetRepository>().As<IPlanetRepository>();
+
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            //builder.RegisterType <StarSystemService>().As<IStarSystemService>();
+            //builder.RegisterType <StarSystemRepository>().As<IStarSystemRepository>();
+
+            var container = builder.Build();
+
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
         }
     }
 }
